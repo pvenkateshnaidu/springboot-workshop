5777d2860050af00c9696de53b149bb6d0d88592
venkatesh <pvenkateshnaidu@gmail.com>
2022-11-27 20:29:16 +0530

    content changed

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -23,8 +22,7 @@ public class CatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
-    @Autowired
-    WebClient.Builder webClientBuilder;
+
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
---
+++ webmobilez-discovery-server/discovery-server/src/test/java/com/webmobilez/discoveryserver/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webmobilez.discoveryserver.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
